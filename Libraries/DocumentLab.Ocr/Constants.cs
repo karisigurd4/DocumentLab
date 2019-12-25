3f4ee1f314eca44c5819e8a28d8e834afda66982
Kári Sigurðsson <karisigurd4@gmail.com>
2019-12-25 23:46:44 +0100

    Allow language to be configured from json file.

--- Libraries/DocumentLab.Ocr/Constants.cs
+++ Libraries/DocumentLab.Ocr/Constants.cs
@@ -9,21 +9,21 @@ namespace DocumentLab.Ocr
   public static class Constants
   {
     // External paths
-    public static string LanguageFilePath => "tessdata";
-    public static string DefaultLanguage => "swe";
     public static string OcrFixDictionaryConfigurationPath = "data\\configuration\\OcrFixDictionary.json";
     public static string OcrConfigurationPath = "data\\configuration\\OcrConfiguration.json";
     public static string HOcrModifiersConfigurationPath = "data\\configuration\\HOcrModifiers.json";
 
     // Configuration objects
-    public static Dictionary<string, int> OcrConfiguration = JsonSerializer.FromFile<Dictionary<string, int>>(Constants.OcrConfigurationPath);
+    public static Dictionary<string, string> OcrConfiguration = JsonSerializer.FromFile<Dictionary<string, string>>(Constants.OcrConfigurationPath);
     public static Dictionary<string, string> OcrFixDictionary = JsonSerializer.FromFile<Dictionary<string, string>>(Constants.OcrFixDictionaryConfigurationPath);
     public static string[] HOcrModifiers = JsonSerializer.FromFile<String[]>(Constants.HOcrModifiersConfigurationPath);
 
     // OCR
-    public static int ResultChunkSize => OcrConfiguration["ResultChunkSize"];
-    public static int TesseractEnginePoolSize => OcrConfiguration["TesseractEnginePoolSize"];
-    public static int NumberOfThreads => OcrConfiguration["NumberOfThreads"];
+    public static string LanguageFilePath => "tessdata";
+    public static string DefaultLanguage => OcrConfiguration["Language"];
+    public static int ResultChunkSize => int.Parse(OcrConfiguration["ResultChunkSize"]);
+    public static int TesseractEnginePoolSize => int.Parse(OcrConfiguration["TesseractEnginePoolSize"]);
+    public static int NumberOfThreads => int.Parse(OcrConfiguration["NumberOfThreads"]);
     public static ImageFormat ConvertBetween => ImageFormat.MemoryBmp;
   }
 }
