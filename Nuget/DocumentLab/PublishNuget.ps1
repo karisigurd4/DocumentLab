0d6e10cd3810074002933956627fb4d910b89b0e
Kári Sigurðsson <karisigurd4@gmail.com>
2019-12-27 20:07:34 +0100

    Delete PublishNuget.ps1

--- Nuget/DocumentLab/PublishNuget.ps1
+++
@@ -1,5 +0,0 @@
-﻿$nugetServer = "https://nuget.org"
-$apiKey = "oy2cfwztwryfk2bp4rma6wd6sp62dxme5lgcvjk25n3hvm"
-
-$package = get-childitem | where {$_.extension -eq ".nupkg"}
-nuget push -Source $nugetServer $package $apiKey
